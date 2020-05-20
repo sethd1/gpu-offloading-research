ef1d507fa024dcb2e8e57abec252ca1f920e8ac2
Dhruv Seth <50122184+sethd1@users.noreply.github.com>
2020-05-19 22:21:38 -0700

    Updated #pragma statement

--- Code/FSNT_GPU_OMP.c
+++ Code/FSNT_GPU_OMP.c
@@ -1,7 +1,7 @@
 // This code was used in order to offload and measure computation speed of the FSNT Loop in nco_rgr.c 
 
 double start = omp_get_wtime();
-        #pragma omp target data map (to: row_dst_adr[0:lnk_idx], var_val_dbl_in[0: var_sz_in*nco_typ_lng(var_typ_rgr)], col_src_adr[0:lnk_idx], wgt_raw[0:lnk_idx]) map(tofrom: var_val_dbl_out[0:var_sz_out*nco_typ_lng(var_typ_rgr)])
+        #pragma omp target data map (to: row_dst_adr[0:lnk_idx], var_val_dbl_in[0: var_sz_in], col_src_adr[0:lnk_idx], wgt_raw[0:lnk_idx]) map(tofrom: var_val_dbl_out[0:var_sz_out])
         if(!sgs_frc_out){
           /* Apply weights */
           if(!has_mss_val){
