b2613e3a19b7b8305e0cd8fd322d645e4059ee77
Dhruv Seth <50122184+sethd1@users.noreply.github.com>
2020-05-26 15:13:42 -0700

    updated pragmas

--- Code/ncremap_GPU_offload.c
+++ Code/ncremap_GPU_offload.c
@@ -7,7 +7,6 @@ if(!sgs_frc_out){
     if(!has_mss_val){
     if(lvl_nbr == 1){
         /* Weight single-level fields without missing values */
-        #pragma omp target teams distribute parallel for
         for(lnk_idx=0;lnk_idx<lnk_nbr;lnk_idx++)
         {
                 var_val_dbl_out[row_dst_adr[lnk_idx]]+=var_val_dbl_in[col_src_adr[lnk_idx]]*wgt_raw[lnk_idx];
@@ -18,10 +17,8 @@ if(!sgs_frc_out){
         val_in_fst=0L;
         val_out_fst=0L;
         /* Weight multi-level fields without missing values */
-        #pragma omp parallel for reduction(+:val_in_fst, val_out_fst)
         for(lvl_idx=0;lvl_idx<lvl_nbr;lvl_idx++){
         //if(nco_dbg_lvl_get() >= nco_dbg_crr) (void)fprintf(fp_stdout,"%s lvl_idx = %d val_in_fst = %li, val_out_fst = %li\n",trv.nm,lvl_idx,val_in_fst,val_out_fst);
-        #pragma omp target teams distribute parallel for
         for(lnk_idx=0;lnk_idx<lnk_nbr;lnk_idx++)
             var_val_dbl_out[row_dst_adr[lnk_idx]+val_out_fst]+=var_val_dbl_in[col_src_adr[lnk_idx]+val_in_fst]*wgt_raw[lnk_idx];
         val_in_fst+=grd_sz_in;
