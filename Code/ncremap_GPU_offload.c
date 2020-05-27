1eed3db197ae40af2140ce016d0f6795896777d4
sethd1 <sethd1@uci.edu>
2020-05-27 12:17:15 -0700

    Update ncremap_GPU_offload.c

--- Code/ncremap_GPU_offload.c
+++ Code/ncremap_GPU_offload.c
@@ -1,30 +1,36 @@
 // Code for both single level and multi level OMP loops that support GPU Offloading
 
+
 /* This first block is for "normal" variables without sub-gridscale fractions */
-#pragma omp target data map (to: row_dst_adr[0:lnk_idx], var_val_dbl_in[0:var_sz_in], col_src_adr[0:lnk_idx], wgt_raw[0:lnk_idx]) map(tofrom: var_val_dbl_out[0:var_sz_out])
+
 if(!sgs_frc_out){
-    /* Apply weights */
-    if(!has_mss_val){
-    if(lvl_nbr == 1){
-        /* Weight single-level fields without missing values */
-        for(lnk_idx=0;lnk_idx<lnk_nbr;lnk_idx++)
-        {
-                var_val_dbl_out[row_dst_adr[lnk_idx]]+=var_val_dbl_in[col_src_adr[lnk_idx]]*wgt_raw[lnk_idx];
-        }
+/* Apply weights */
+if(!has_mss_val){
+if(lvl_nbr == 1){
+    /* Weight single-level fields without missing values */
+    #pragma omp target data map (to: row_dst_adr[0:lnk_idx], var_val_dbl_in[0:var_sz_in], col_src_adr[0:lnk_idx], wgt_raw[0:lnk_idx]) map(tofrom: var_val_dbl_out[0:var_sz_out])
+    #pragma omp target teams distribute parallel for
+    for(lnk_idx=0;lnk_idx<lnk_nbr;lnk_idx++)
+    {
+        var_val_dbl_out[row_dst_adr[lnk_idx]]+=var_val_dbl_in[col_src_adr[lnk_idx]]*wgt_raw[lnk_idx];
     }
-    else{
-        #pragma omp target data map (to: row_dst_adr[0:lnk_idx], var_val_dbl_in[0:var_sz_in], col_src_adr[0:lnk_idx], wgt_raw[0:lnk_idx]) map(tofrom: var_val_dbl_out[0:var_sz_out])
-        val_in_fst=0L;
-        val_out_fst=0L;
-        /* Weight multi-level fields without missing values */
-        for(lvl_idx=0;lvl_idx<lvl_nbr;lvl_idx++){
-        //if(nco_dbg_lvl_get() >= nco_dbg_crr) (void)fprintf(fp_stdout,"%s lvl_idx = %d val_in_fst = %li, val_out_fst = %li\n",trv.nm,lvl_idx,val_in_fst,val_out_fst);
-        for(lnk_idx=0;lnk_idx<lnk_nbr;lnk_idx++)
-            var_val_dbl_out[row_dst_adr[lnk_idx]+val_out_fst]+=var_val_dbl_in[col_src_adr[lnk_idx]+val_in_fst]*wgt_raw[lnk_idx];
-        val_in_fst+=grd_sz_in;
-        val_out_fst+=grd_sz_out;
-        }
+}
+else{
+    double start = omp_get_wtime();
+    val_in_fst=0L;
+    val_out_fst=0L;
+    /* Weight multi-level fields without missing values */
+    #pragma omp target data map (to: row_dst_adr[0:lnk_idx], var_val_dbl_in[0:var_sz_in], col_src_adr[0:lnk_idx], wgt_raw[0:lnk_idx]) map(tofrom: var_val_dbl_out[0:var_sz_out])
+    #pragma omp parallel for reduction(+:val_in_fst, val_out_fst)
+    for(lvl_idx=0;lvl_idx<lvl_nbr;lvl_idx++){
+    //if(nco_dbg_lvl_get() >= nco_dbg_crr) (void)fprintf(fp_stdout,"%s lvl_idx = %d val_in_fst = %li, val_out_fst = %li\n",trv.nm,lvl_idx,val_in_fst,val_out_fst);
+    #pragma omp target teams distribute parallel for
+    for(lnk_idx=0;lnk_idx<lnk_nbr;lnk_idx++)
+        var_val_dbl_out[row_dst_adr[lnk_idx]+val_out_fst]+=var_val_dbl_in[col_src_adr[lnk_idx]+val_in_fst]*wgt_raw[lnk_idx];
+    val_in_fst+=grd_sz_in;
+    val_out_fst+=grd_sz_out;
+    }
 
 // Computation Speeds for CPU/GPU for the entire data set (on average of 11 runs omitting the 1st run)
-// CPU Computation Speed = 35.282 seconds 
-// GPU Computation Speed = 5.577 seconds
+// CPU Computation Speed = 43.9541 seconds 
+// GPU Computation Speed = 5.265 seconds
